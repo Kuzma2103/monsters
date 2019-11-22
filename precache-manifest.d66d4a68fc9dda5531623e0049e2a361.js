5ee3c8b584c3053e10890f4b3ecbbc6ba2ca78bf
unknown <ivankuzmanovic87@gmial.com>
2019-11-22 14:14:55 +0100

    Updates

---
+++ precache-manifest.d66d4a68fc9dda5531623e0049e2a361.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14553533fb53cfa38b6ae6b5e0ad2d83",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "e9ce0f11cafb60563c49",
+    "url": "/monsters/static/css/main.b5730d86.chunk.css"
+  },
+  {
+    "revision": "44918c832c22c405b949",
+    "url": "/monsters/static/js/2.b80f281c.chunk.js"
+  },
+  {
+    "revision": "e9ce0f11cafb60563c49",
+    "url": "/monsters/static/js/main.7f8b9b76.chunk.js"
+  },
+  {
+    "revision": "61b7034a091e44313d88",
+    "url": "/monsters/static/js/runtime-main.5ec979ac.js"
+  }
+]);
